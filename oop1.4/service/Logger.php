41f88def379d4b941ba4aa1f83f11b44f08f7bbd
StijnGyssens <stijn.gyssens@gmail.com>
2022-03-04 18:13:34 +0100

    oef 1.4 klaar

--- oop1.4/service/Logger.php
+++ oop1.4/service/Logger.php
@@ -5,20 +5,50 @@ class Logger
     private $fp;
     private $logfile;
 
-    public function __constructor(){
-        $this->fp = fopen($_SERVER["DOCUMENT_ROOT"] . "\oop1.4\log\log.txt","c+");
-        $this->logfile="log.txt";
+    public function __construct(){
+        $this->setFp(fopen($_SERVER["DOCUMENT_ROOT"] . "\oop1.4\log\log.txt","a+")) ;
+        $this->logfile=$_SERVER["DOCUMENT_ROOT"] . "\oop1.4\log\log.txt";
     }
 
     public function Log($msg){
-        var_dump($this->fp); //geeft NULL terug
-        print "<br/>";
-        var_dump(fopen($_SERVER["DOCUMENT_ROOT"] . "\oop1.4\log\log.txt","c+"));//geeft resource(18) of type (stream) terug
         fwrite($this->fp,date("d-m-Y H:i:s").': '.$msg.'\r\n' );
     }
 
     public function ShowLog(){
         return file_get_contents($this->logfile);
     }
+
+    /**
+     * @return mixed
+     */
+    public function getFp()
+    {
+        return $this->fp;
+    }
+
+    /**
+     * @param mixed $fp
+     */
+    public function setFp($fp): void
+    {
+        $this->fp = $fp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLogfile()
+    {
+        return $this->logfile;
+    }
+
+    /**
+     * @param mixed $logfile
+     */
+    public function setLogfile($logfile): void
+    {
+        $this->logfile = $logfile;
+    }
+
 
 }
